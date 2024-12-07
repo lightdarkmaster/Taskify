9760fcf381c48a7100b840c356f5e9939bad5942
lightdarkmaster <79690406+lightdarkmaster@users.noreply.github.com>
2024-12-07 21:40:09 +0800

    Configure Notes and Task Details

--- lib/pages/note_details.dart
+++ lib/pages/note_details.dart
@@ -10,7 +10,7 @@ class NoteDetailsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Note Details'),
+        title: const Text('Note Details', style: TextStyle(fontFamily: 'Monserat'),),
         backgroundColor: headerColor,
       ),
       body: Padding(
@@ -33,28 +33,9 @@ class NoteDetailsPage extends StatelessWidget {
                         style: const TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 22,
+                          fontFamily: 'Monserat',
                         ),
                       ),
-                    ),
-                    PopupMenuButton<String>(
-                      onSelected: (value) {
-                        // Handle menu selection
-                        if (value == 'Edit') {
-                          // Handle edit action
-                        } else if (value == 'Delete') {
-                          // Handle delete action
-                        }
-                      },
-                      itemBuilder: (context) => [
-                        const PopupMenuItem(
-                          value: 'Edit',
-                          child: Text('Edit'),
-                        ),
-                        const PopupMenuItem(
-                          value: 'Delete',
-                          child: Text('Delete'),
-                        ),
-                      ],
                     ),
                   ],
                 ),
@@ -65,7 +46,7 @@ class NoteDetailsPage extends StatelessWidget {
                 const SizedBox(height: 5),
                 Text(
                   note['description'],
-                  style: const TextStyle(fontSize: 18),
+                  style: const TextStyle(fontSize: 18, fontFamily: 'Monserat'),
                 ),
               ],
             ),
--- lib/pages/task_details.dart
+++ lib/pages/task_details.dart
@@ -10,7 +10,7 @@ class TaskDetailsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Task Details'),
+        title: const Text('Task Details', style: TextStyle(fontFamily: 'Monserat')),
         backgroundColor: headerColor,
       ),
       body: Padding(
@@ -33,28 +33,9 @@ class TaskDetailsPage extends StatelessWidget {
                         style: const TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 22,
+                          fontFamily: 'Monserat'
                         ),
                       ),
-                    ),
-                    PopupMenuButton<String>(
-                      onSelected: (value) {
-                        // Handle menu selection
-                        if (value == 'Edit') {
-                          // Handle edit action
-                        } else if (value == 'Delete') {
-                          // Handle delete action
-                        }
-                      },
-                      itemBuilder: (context) => [
-                        const PopupMenuItem(
-                          value: 'Edit',
-                          child: Text('Edit'),
-                        ),
-                        const PopupMenuItem(
-                          value: 'Delete',
-                          child: Text('Delete'),
-                        ),
-                      ],
                     ),
                   ],
                 ),
@@ -65,7 +46,7 @@ class TaskDetailsPage extends StatelessWidget {
                 const SizedBox(height: 5),
                 Text(
                   task['description'],
-                  style: const TextStyle(fontSize: 18),
+                  style: const TextStyle(fontSize: 18, fontFamily: 'Monserat'),
                 ),
               ],
             ),
